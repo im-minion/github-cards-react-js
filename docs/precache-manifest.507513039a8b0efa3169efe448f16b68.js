0d5d7115c9c1b45f8d868afddc500bc760361027
im-minion <vaibhavminiyar@gmail.com>
2020-10-07 20:17:46 +0530

    build is critical :p

--- docs/precache-manifest.507513039a8b0efa3169efe448f16b68.js
+++ docs/precache-manifest.507513039a8b0efa3169efe448f16b68.js
@@ -5,22 +5,22 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "745ce94ed441d4215711",
-    "url": "/static/css/main.548b4767.chunk.css"
+    "url": "/main.548b4767.chunk.css"
   },
   {
     "revision": "1b3d30e8f366794cf6bf",
-    "url": "/static/js/2.9638b5c0.chunk.js"
+    "url": "/2.9638b5c0.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.9638b5c0.chunk.js.LICENSE.txt"
+    "url": "/2.9638b5c0.chunk.js.LICENSE.txt"
   },
   {
     "revision": "745ce94ed441d4215711",
-    "url": "/static/js/main.04bee16b.chunk.js"
+    "url": "/main.04bee16b.chunk.js"
   },
   {
     "revision": "c0ce14d854050fce961b",
-    "url": "/static/js/runtime-main.2b789e8c.js"
+    "url": "/runtime-main.2b789e8c.js"
   }
 ]);
