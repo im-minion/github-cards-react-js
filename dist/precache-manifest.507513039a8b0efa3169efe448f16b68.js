5141a77d90ae276b6633eff887602a2ca3920590
im-minion <vaibhavminiyar@gmail.com>
2020-10-07 20:01:03 +0530

    build

---
+++ dist/precache-manifest.507513039a8b0efa3169efe448f16b68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d4f8df6dc89b515d9e64d3aeacd1dc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "745ce94ed441d4215711",
+    "url": "/static/css/main.548b4767.chunk.css"
+  },
+  {
+    "revision": "1b3d30e8f366794cf6bf",
+    "url": "/static/js/2.9638b5c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9638b5c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "745ce94ed441d4215711",
+    "url": "/static/js/main.04bee16b.chunk.js"
+  },
+  {
+    "revision": "c0ce14d854050fce961b",
+    "url": "/static/js/runtime-main.2b789e8c.js"
+  }
+]);
